6337fb963142e21dde419ca70d194ae108bed30e
SaberElsayed <saberelsayed3@gmail.com>
2026-09-03 11:19:51 +0400

    Add files via upload

---
+++ Week2_Lec2_SecondsToMinutes.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int seconds;
+	// ask user to enter the number of seconds
+	cout<< "Please enter the number of seconds: ";
+	//get input from user
+	cin>> seconds;
+	//cal number of minutes and remaining seconds
+	int min= seconds/60;
+	int remainingSeconds= seconds%60;
+	//output results
+	cout << "The total number of minutes in " << seconds <<	" is " << min << " and the reamining seconds are " << remainingSeconds;
+
+
+
+
+}
